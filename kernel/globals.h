fd68aba2c9b26f6afd3d28117d331ef6e75ee924
alex <ajovedel@andrew.cmu.edu>
2015-11-07 13:41:23 -0500

    changed TIME_UNITS_ELAPSED to be volatile and fiexed the TIME_RES_CYCLES calculation

--- kernel/globals.h
+++ kernel/globals.h
@@ -6,7 +6,7 @@
 #define IRQ_VECTOR_ADDR (unsigned *) 0x18
 #define OS_CLK_SPEED (unsigned long) 3250000
 #define TIME_RES_MS (unsigned long) 10
-#define TIME_RES_CYCLES (OS_CLK_SPEED * (0.01 * TIME_RES_MS))
+#define TIME_RES_CYCLES (unsigned long) (OS_CLK_SPEED * (0.001 * TIME_RES_MS))
 
 void *swi_handler();
 void *irq_handler();
@@ -16,4 +16,4 @@ void irq_setup();
 void timer_setup();
 void interrupt_setup();
 
-extern unsigned long TIME_UNITS_ELAPSED;
+extern volatile unsigned long TIME_UNITS_ELAPSED;
