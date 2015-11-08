76b5d1e1860ea561fb1ebee636e52f1b9298c6af
alex <ajovedel@andrew.cmu.edu>
2015-11-07 19:11:33 -0500

    deleted comments and fixed print message

--- kernel/c_irq_handler.c
+++ kernel/c_irq_handler.c
@@ -6,12 +6,9 @@
 #include "globals.h"
 
 void c_irq_handler(){
-//	printf("I AM IN C_IRQ_HANDLER!!\n");
 
 	// check if timer interrupt has ocurred
 	if(reg_read(OSTMR_OSSR_ADDR) && OSTMR_OSSR_M0){
-	
-//		printf("Timer interrupt has occured\n");
 
 		// the OSMR0 registers has to be reloaded with OSCR + 32500 (OSCR + 10ms)
 		reg_write(OSTMR_OSMR_ADDR(0), (reg_read(OSTMR_OSCR_ADDR) + TIME_RES_CYCLES));
@@ -20,12 +17,9 @@ void c_irq_handler(){
 		reg_set(OSTMR_OSSR_ADDR, OSTMR_OSSR_M0);
 
 		TIME_UNITS_ELAPSED += 1;
-
-		//printf("TIME_ELAPSED: %lu\n", TIME_UNITS_ELAPSED);
 	}
 	
 	// if another type of interrupt has ocurred, we ignore it
 	else
-		printf("idk why im here\n");
-
+		printf("Unknown IRQ triggered\n");
 }
