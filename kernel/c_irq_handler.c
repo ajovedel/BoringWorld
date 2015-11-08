7b77b24cf2fdc3d876ef7f8b47fa3db5b7cf4aa4
alex <ajovedel@andrew.cmu.edu>
2015-11-07 20:53:12 -0500

    added time drift to be taken into consideration when calculating next match

--- kernel/c_irq_handler.c
+++ kernel/c_irq_handler.c
@@ -7,11 +7,16 @@
 
 void c_irq_handler(){
 
+	unsigned long drift;
+
 	// check if timer interrupt has ocurred
 	if(reg_read(OSTMR_OSSR_ADDR) && OSTMR_OSSR_M0){
 
+		// calculate drift
+		drift = reg_read(OSTMR_OSCR_ADDR) - reg_read(OSTMR_OSMR_ADDR(0));
+	
 		// the OSMR0 registers has to be reloaded with OSCR + 32500 (OSCR + 10ms)
-		reg_write(OSTMR_OSMR_ADDR(0), (reg_read(OSTMR_OSCR_ADDR) + TIME_RES_CYCLES));
+		reg_write(OSTMR_OSMR_ADDR(0), (reg_read(OSTMR_OSCR_ADDR) + TIME_RES_CYCLES - drift));
 
 		// the OSSR[0] register has to be reset (acknowledged)
 		reg_set(OSTMR_OSSR_ADDR, OSTMR_OSSR_M0);
